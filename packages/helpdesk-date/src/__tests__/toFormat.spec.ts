7c5c3f7532b25623fe4e12f6e429215e9cba2d1e
molgga84 <molgga84@gmail.com>
2022-08-09 20:37:06 +0900

    test timezone 수정

--- packages/helpdesk-date/src/__tests__/toFormat.spec.ts
+++ packages/helpdesk-date/src/__tests__/toFormat.spec.ts
@@ -1,40 +1,51 @@
-import { toFormat } from '../toFormat';
+import { toFormat } from '../toFormat'
 
 describe('toFormat', () => {
-  const at1 = 1553146437000; // new Date("2019-03-21 14:33:57").getTime();
-  const at2 = 1553146437; // unixtime
+  const testDate = new Date('2019-03-21T14:33:57.000+09:00')
+  const at1 = testDate.getTime()
+  const at2 = Math.floor(at1 / 1000) // unixtime
 
   it("invalid 한 날짜는 공백('')으로 반환 되어야 합니다.", () => {
-    expect(toFormat(null)).toBe('');
-    expect(toFormat(undefined)).toBe('');
-    expect(toFormat('invalid')).toBe('');
-  });
+    expect(toFormat(null)).toBe('')
+    expect(toFormat(undefined)).toBe('')
+    expect(toFormat('invalid')).toBe('')
+  })
 
   it('변경 가능한 날짜는 기본 포맷으로 출력되어야 합니다.', () => {
-    expect(toFormat(at1)).toBe('2019-03-21 14:33:57');
-  });
+    expect(toFormat(at1)).toBe('2019-03-21 14:33:57')
+  })
 
   it('format 옵션에 지정된 형식으로 출력되어야 합니다.', () => {
-    expect(toFormat(at1, { format: 'YYYY-MM-DD(d)' })).toBe('2019-03-21(목)');
-    expect(toFormat(at1, { format: 'hh:mm:ss YYYY/MM/DD' })).toBe('14:33:57 2019/03/21');
-  });
+    expect(toFormat(at1, { format: 'YYYY-MM-DD(d)' })).toBe('2019-03-21(목)')
+    expect(toFormat(at1, { format: 'hh:mm:ss YYYY/MM/DD' })).toBe(
+      '14:33:57 2019/03/21',
+    )
+  })
 
   it('multiple 옵션에 지정된 값만큼 곱해진 값으로 출력되어야 합니다.', () => {
-    expect(toFormat(at2, { multiple: 1000 })).toBe('2019-03-21 14:33:57');
-  });
+    expect(toFormat(at2, { multiple: 1000 })).toBe('2019-03-21 14:33:57')
+  })
 
   it('labelDays 옵션에 지정된 날짜형식으로 출력되어야 합니다.', () => {
     expect(
       toFormat(at1, {
-        labelDays: ['일요일!', '월요일!', '화요일!', '수요일!', '목요일!', '금요일!', '토요일!'],
+        labelDays: [
+          '일요일!',
+          '월요일!',
+          '화요일!',
+          '수요일!',
+          '목요일!',
+          '금요일!',
+          '토요일!',
+        ],
         format: 'YYYY-MM-DD (d)',
-      })
-    ).toBe('2019-03-21 (목요일!)');
-  });
+      }),
+    ).toBe('2019-03-21 (목요일!)')
+  })
 
   it('alternative 옵션 지정시 invalid 한 날짜는 지정된 alternative 값으로 반환 되어야 합니다.', () => {
-    expect(toFormat(null, { alternative: '-' })).toBe('-');
-    expect(toFormat('', { alternative: 'Unknown' })).toBe('Unknown');
-    expect(toFormat(at1, { alternative: 'Unknown' })).not.toBe('Unknown');
-  });
-});
+    expect(toFormat(null, { alternative: '-' })).toBe('-')
+    expect(toFormat('', { alternative: 'Unknown' })).toBe('Unknown')
+    expect(toFormat(at1, { alternative: 'Unknown' })).not.toBe('Unknown')
+  })
+})
