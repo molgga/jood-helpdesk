2fa15e696e4337b00e9cdee6a024516e6769aeb9
molgga84 <molgga84@gmail.com>
2022-08-13 10:38:57 +0900

    demo(timer): 타이머 데모 추가

--- packages/demo/src/router/index.ts
+++ packages/demo/src/router/index.ts
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: () => import('@/views/Home.vue'),
+  },
+  {
+    path: '/timer',
+    name: 'Timer',
+    component: () => import('@/views/Timer.vue'),
   },
 ];
 
---
+++ packages/demo/src/views/Timer.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <div class="time">
+      <dl>
+        <dt>SecondTimer</dt>
+        <dd>{{ viewState.labelSecond }}</dd>
+      </dl>
+    </div>
+    <div class="time">
+      <dl>
+        <dt>MinuteTimer</dt>
+        <dd>{{ viewState.labelMinute }}</dd>
+      </dl>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, onUnmounted, reactive } from 'vue';
+import { SecondTimer, MinuteTimer } from '@jood/helpdesk-timer';
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      secondDate: null, //new Date(),
+      minuteDate: null, //new Date(),
+    });
+
+    const viewState = computed(() => {
+      const { secondDate, minuteDate } = state;
+      const labelSecond = secondDate.toLocaleString();
+      const labelMinute = minuteDate.toLocaleString();
+      return {
+        labelSecond,
+        labelMinute,
+      };
+    });
+
+    const serverTime = Date.now(); // - 1000 * 60 * 60 * 2;
+    const secondTimer = new SecondTimer();
+    const secondListener = secondTimer.observe().subscribe(() => {
+      console.log('SeconTimer::now:', new Date().toLocaleString());
+      console.log('SeconTimer::gap:', new Date(serverTime + secondTimer.getExecuteGap()).toLocaleString());
+      state.secondDate = new Date(serverTime + secondTimer.getExecuteGap());
+    });
+    secondTimer.start();
+
+    const minuteTimer = new MinuteTimer();
+    const minuteListener = minuteTimer.observe().subscribe(() => {
+      console.log(' ################################ ');
+      console.log('MinuteTimer::now:', new Date().toLocaleString());
+      console.log('MinuteTimer::gap:', new Date(serverTime + minuteTimer.getExecuteGap()).toLocaleString());
+      console.log(' ================================ ');
+      state.minuteDate = new Date(serverTime + minuteTimer.getExecuteGap());
+    });
+    minuteTimer.start();
+
+    console.log(new Date().toLocaleString());
+    state.secondDate = new Date(serverTime + secondTimer.getExecuteGap());
+    state.minuteDate = new Date(serverTime + minuteTimer.getExecuteGap());
+
+    onUnmounted(() => {
+      secondListener.unsubscribe();
+      minuteListener.unsubscribe();
+    });
+    return {
+      state,
+      viewState,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.time {
+  padding: 20px;
+  font-weight: bold;
+  color: #222;
+  dt {
+    padding: 0;
+    margin: 0;
+    font-size: 20px;
+  }
+  dd {
+    padding: 0;
+    margin: 0;
+    font-size: 40px;
+  }
+}
+</style>
