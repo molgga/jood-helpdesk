3916d45d3cc50cdcf77c0437e2d15b4a38ebdf8d
molgga84 <molgga84@gmail.com>
2023-12-27 08:31:27 +0900

    fix(date): 테스트 코드 보완

--- packages/helpdesk-date/src/__tests__/toDiffHMS.spec.ts
+++ packages/helpdesk-date/src/__tests__/toDiffHMS.spec.ts
@@ -4,8 +4,8 @@ describe('helpdesk-date: toDiffHMS', () => {
   it('두 시간 사이의 남은 시간을 반환해야 합니다.', () => {
     expect(
       toDiffHMS(
-        new Date('2022-07-25 07:32:33').getTime(),
-        new Date('2022-07-25 23:59:59').getTime()
+        new Date('2022-07-25T07:32:33').getTime(),
+        new Date('2022-07-25T23:59:59').getTime()
       )
     ).toEqual({ hour: 16, minute: 27, second: 26 });
   });
@@ -13,8 +13,8 @@ describe('helpdesk-date: toDiffHMS', () => {
   it('두 시간 사이의 남은 시간이 일자(day)가 넘어가는 경우 일자는 24시간을 곱해서 반환해야 합니다.', () => {
     expect(
       toDiffHMS(
-        new Date('2022-07-25 21:59:39').getTime(),
-        new Date('2022-07-26 23:59:59').getTime()
+        new Date('2022-07-25T21:59:39').getTime(),
+        new Date('2022-07-26T23:59:59').getTime()
       )
     ).toEqual({ hour: 26, minute: 0, second: 20 });
   });
--- packages/helpdesk-date/src/__tests__/toPast.spec.ts
+++ packages/helpdesk-date/src/__tests__/toPast.spec.ts
@@ -1,21 +1,21 @@
 import { toPast } from '../toPast';
 
 describe('helpdesk-date: toPast', () => {
-  const now = new Date('2019-03-12 08:10:20').getTime();
+  const now = new Date('2023-03-12T08:10:20').getTime();
 
   test('기본 옵션 테스트', () => {
-    expect(toPast(now, new Date('2019-03-12 08:09:21').getTime())).toBe('방금 전');
-    expect(toPast(now, new Date('2019-03-12 08:00:20').getTime())).toBe('10분 전');
-    expect(toPast(now, new Date('2019-03-12 07:10:20').getTime())).toBe('1시간 전');
-    expect(toPast(now, new Date('2019-03-11 08:10:21').getTime())).toBe('23시간 전');
-    expect(toPast(now, new Date('2019-03-10 08:10:20').getTime())).toBe('2일 전');
+    expect(toPast(now, new Date('2023-03-12T08:09:21').getTime())).toBe('방금 전');
+    expect(toPast(now, new Date('2023-03-12T08:00:20').getTime())).toBe('10분 전');
+    expect(toPast(now, new Date('2023-03-12T07:10:20').getTime())).toBe('1시간 전');
+    expect(toPast(now, new Date('2023-03-11T08:10:21').getTime())).toBe('23시간 전');
+    expect(toPast(now, new Date('2023-03-10T08:10:20').getTime())).toBe('2일 전');
     expect(
-      toPast(now, new Date('2019-02-10 08:10:19').getTime(), { format: 'YYYY년 MM월 DD일' })
+      toPast(now, new Date('2023-02-10T08:10:19').getTime(), { format: 'YYYY년 MM월 DD일' })
     ).toBe('2019년 02월 10일');
   });
 
   test('옵션 지정시 값 테스트', () => {
-    const now = new Date('2019-03-12 08:10:20').getTime();
+    const now = new Date('2023-03-12T08:10:20').getTime();
     const options = {
       justLabel: '방금',
       minuteLabel: '분',
@@ -23,11 +23,11 @@ describe('helpdesk-date: toPast', () => {
       dayLabel: '일',
       alternative: '알 수 없음',
     };
-    expect(toPast(now, new Date('2019-03-12 08:09:21').getTime(), options)).toBe('방금');
-    expect(toPast(now, new Date('2019-03-12 08:00:20').getTime(), options)).toBe('10분');
-    expect(toPast(now, new Date('2019-03-12 07:10:20').getTime(), options)).toBe('1시간');
-    expect(toPast(now, new Date('2019-03-11 08:10:21').getTime(), options)).toBe('23시간');
-    expect(toPast(now, new Date('2019-03-10 08:10:20').getTime(), options)).toBe('2일');
+    expect(toPast(now, new Date('2023-03-12T08:09:21').getTime(), options)).toBe('방금');
+    expect(toPast(now, new Date('2023-03-12T08:00:20').getTime(), options)).toBe('10분');
+    expect(toPast(now, new Date('2023-03-12T07:10:20').getTime(), options)).toBe('1시간');
+    expect(toPast(now, new Date('2023-03-11T08:10:21').getTime(), options)).toBe('23시간');
+    expect(toPast(now, new Date('2023-03-10T08:10:20').getTime(), options)).toBe('2일');
     expect(toPast(now, null, options)).toBe('알 수 없음');
   });
 
@@ -36,11 +36,11 @@ describe('helpdesk-date: toPast', () => {
       justMax: 60 * 60,
       justLabel: '방금',
     };
-    const now = new Date('2019-03-12 08:10:20').getTime();
-    expect(toPast(now, new Date('2019-03-12 08:09:21').getTime(), options)).toBe('방금');
-    expect(toPast(now, new Date('2019-03-12 08:00:20').getTime(), options)).toBe('방금');
-    expect(toPast(now, new Date('2019-03-12 07:10:21').getTime(), options)).toBe('방금');
-    expect(toPast(now, new Date('2019-03-12 07:10:20').getTime(), options)).toBe('1시간 전');
+    const now = new Date('2023-03-12T08:10:20').getTime();
+    expect(toPast(now, new Date('2023-03-12T08:09:21').getTime(), options)).toBe('방금');
+    expect(toPast(now, new Date('2023-03-12T08:00:20').getTime(), options)).toBe('방금');
+    expect(toPast(now, new Date('2023-03-12T07:10:21').getTime(), options)).toBe('방금');
+    expect(toPast(now, new Date('2023-03-12T07:10:20').getTime(), options)).toBe('1시간 전');
   });
 
   test('옵션 minuteMax - n분 체크시간 테스트', () => {
@@ -49,16 +49,16 @@ describe('helpdesk-date: toPast', () => {
       minuteMax: 60 * 60,
       minuteLabel: '분 전',
     };
-    const now = new Date('2019-03-12 08:10:20').getTime();
-    expect(toPast(now, new Date('2019-03-12 08:09:21').getTime(), options)).toBe('0분 전');
-    expect(toPast(now, new Date('2019-03-12 08:09:19').getTime(), options)).toBe('1분 전');
-    expect(toPast(now, new Date('2019-03-12 08:00:20').getTime(), options)).toBe('10분 전');
-    expect(toPast(now, new Date('2019-03-12 07:50:20').getTime(), options)).toBe('20분 전');
-    expect(toPast(now, new Date('2019-03-12 07:40:20').getTime(), options)).toBe('30분 전');
-    expect(toPast(now, new Date('2019-03-12 07:30:20').getTime(), options)).toBe('40분 전');
-    expect(toPast(now, new Date('2019-03-12 07:20:20').getTime(), options)).toBe('50분 전');
-    expect(toPast(now, new Date('2019-03-12 07:10:21').getTime(), options)).toBe('59분 전');
-    expect(toPast(now, new Date('2019-03-12 07:10:20').getTime(), options)).toBe('1시간 전');
+    const now = new Date('2023-03-12T08:10:20').getTime();
+    expect(toPast(now, new Date('2023-03-12T08:09:21').getTime(), options)).toBe('0분 전');
+    expect(toPast(now, new Date('2023-03-12T08:09:19').getTime(), options)).toBe('1분 전');
+    expect(toPast(now, new Date('2023-03-12T08:00:20').getTime(), options)).toBe('10분 전');
+    expect(toPast(now, new Date('2023-03-12T07:50:20').getTime(), options)).toBe('20분 전');
+    expect(toPast(now, new Date('2023-03-12T07:40:20').getTime(), options)).toBe('30분 전');
+    expect(toPast(now, new Date('2023-03-12T07:30:20').getTime(), options)).toBe('40분 전');
+    expect(toPast(now, new Date('2023-03-12T07:20:20').getTime(), options)).toBe('50분 전');
+    expect(toPast(now, new Date('2023-03-12T07:10:21').getTime(), options)).toBe('59분 전');
+    expect(toPast(now, new Date('2023-03-12T07:10:20').getTime(), options)).toBe('1시간 전');
   });
 
   test('옵션 hourMax - n시간 체크시간 테스트', () => {
@@ -69,14 +69,14 @@ describe('helpdesk-date: toPast', () => {
       hourMax: 60 * 60 * 48,
       hourLabel: '시간 전',
     };
-    const now = new Date('2019-03-12 08:10:20').getTime();
-    expect(toPast(now, new Date('2019-03-12 07:20:20').getTime(), options)).toBe('50분 전');
-    expect(toPast(now, new Date('2019-03-12 07:10:21').getTime(), options)).toBe('59분 전');
-    expect(toPast(now, new Date('2019-03-12 07:10:20').getTime(), options)).toBe('1시간 전');
-    expect(toPast(now, new Date('2019-03-11 07:10:21').getTime(), options)).toBe('24시간 전');
-    expect(toPast(now, new Date('2019-03-11 07:10:20').getTime(), options)).toBe('25시간 전');
-    expect(toPast(now, new Date('2019-03-10 08:10:21').getTime(), options)).toBe('47시간 전');
-    expect(toPast(now, new Date('2019-03-10 08:10:20').getTime(), options)).toBe('2일 전');
+    const now = new Date('2023-03-12T08:10:20').getTime();
+    expect(toPast(now, new Date('2023-03-12T07:20:20').getTime(), options)).toBe('50분 전');
+    expect(toPast(now, new Date('2023-03-12T07:10:21').getTime(), options)).toBe('59분 전');
+    expect(toPast(now, new Date('2023-03-12T07:10:20').getTime(), options)).toBe('1시간 전');
+    expect(toPast(now, new Date('2023-03-11T07:10:21').getTime(), options)).toBe('24시간 전');
+    expect(toPast(now, new Date('2023-03-11T07:10:20').getTime(), options)).toBe('25시간 전');
+    expect(toPast(now, new Date('2023-03-10T08:10:21').getTime(), options)).toBe('47시간 전');
+    expect(toPast(now, new Date('2023-03-10T08:10:20').getTime(), options)).toBe('2일 전');
   });
 
   test('옵션 dayMax - n일 체크시간 테스트', () => {
@@ -90,11 +90,11 @@ describe('helpdesk-date: toPast', () => {
       dayLabel: '일 전',
       format: 'YYYY년 MM월 DD일',
     };
-    const now = new Date('2019-03-12 08:10:20').getTime();
-    expect(toPast(now, new Date('2019-03-11 08:10:20').getTime(), options)).toBe('1일 전');
-    expect(toPast(now, new Date('2019-03-10 08:10:20').getTime(), options)).toBe('2일 전');
-    expect(toPast(now, new Date('2019-03-09 08:10:20').getTime(), options)).toBe('3일 전');
-    expect(toPast(now, new Date('2019-03-09 08:10:19').getTime(), options)).toBe(
+    const now = new Date('2023-03-12T08:10:20').getTime();
+    expect(toPast(now, new Date('2023-03-11T08:10:20').getTime(), options)).toBe('1일 전');
+    expect(toPast(now, new Date('2023-03-10T08:10:20').getTime(), options)).toBe('2일 전');
+    expect(toPast(now, new Date('2023-03-09T08:10:20').getTime(), options)).toBe('3일 전');
+    expect(toPast(now, new Date('2023-03-09T08:10:19').getTime(), options)).toBe(
       '2019년 03월 09일'
     );
   });
