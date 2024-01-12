c0095db4718de8a79fc53b075edc76f7da06bb45
molgga <molgga@users.noreply.github.com>
2024-01-12 04:35:16 +0000

    deploy: f70b86ab1d911984abdf61fe2d5f8772cdaff39a

---
+++ assets/module_hangul-core_characterMatrix.md.RoH09f0d.js
@@ -0,0 +1,16 @@
+import{_ as s,c as a,o as i,U as t}from"./chunks/framework.WU9gZgzV.js";const q=JSON.parse('{"title":"hangul-core","description":"","frontmatter":{},"headers":[],"relativePath":"module/hangul-core/characterMatrix.md","filePath":"module/hangul-core/characterMatrix.md"}'),n={name:"module/hangul-core/characterMatrix.md"},e=t(`<h1 id="hangul-core" tabindex="-1">hangul-core <a class="header-anchor" href="#hangul-core" aria-label="Permalink to &quot;hangul-core&quot;">​</a></h1><h2 id="charactermatrix" tabindex="-1">characterMatrix <a class="header-anchor" href="#charactermatrix" aria-label="Permalink to &quot;characterMatrix&quot;">​</a></h2><p>초/중/종성을 분리한다.</p><div class="language-ts vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">ts</span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">characterMatrix</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">(</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&#39;안녕하세요! Hello~&#39;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">)</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">// [</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;ㅇ&quot;, &quot;ㅏ&quot;, &quot;ㄴ&quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;ㄴ&quot;, &quot;ㅕ&quot;, &quot;ㅇ&quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;ㅎ&quot;, &quot;ㅏ&quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;ㅅ&quot;, &quot;ㅔ&quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;ㅇ&quot;, &quot;ㅛ&quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;!&quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot; &quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;H&quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;e&quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;l&quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;l&quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;o&quot; ],</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">//   [ &quot;~&quot; ]</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">// ]</span></span></code></pre></div>`,4),l=[e];function h(p,o,r,c,u,k){return i(),a("div",null,l)}const g=s(n,[["render",h]]);export{q as __pageData,g as default};
